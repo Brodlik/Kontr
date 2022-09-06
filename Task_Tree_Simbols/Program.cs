560a40b746be7881215456c33f6ced58650316a7
Svetlana <brodlik@me.com>
2022-09-06 19:30:57 +0300

    Input code

---
+++ Task_Tree_Simbols/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine();
+string[] sourseArray = {"a","25524555", "-56", "logi", "mine God", "dog", "62252", "+698524" };
+
+Console.WriteLine($"Исходный массив: {'['+string.Join(" ,", sourseArray)+']'}");
+
+void CutArray(string[] array)
+{
+    string[] cutArray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            cutArray[i] = array[i];
+        }
+    } 
+    Console.Write($"Сокращенный массив: {'['+string.Join(" ", cutArray)+']'}");
+}
+CutArray(sourseArray);
+
